f79d2fe481f046f87d1e126e9645cddae4e8819e
iabhinavrajput <abhinavrajput207@gmail.com>
2025-06-25 17:36:28 +0530

    Implement CRUD functionality for notes with views for adding, editing, and deleting notes

--- notes/views.py
+++ notes/views.py
@@ -1,3 +1,36 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 
-# Create your views here.
+from .models import Note
+from .forms import NoteForm
+
+def home(request):
+    notes = Note.objects.all().order_by('-created_at')
+    return render(request, 'notes/home.html', {'notes':notes})
+
+def add_note(request):
+    if request.method == 'POST':
+        form = NoteForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    else:
+        form = NoteForm()
+    return render(request, 'notes/add_note.html', {'form' : form})
+
+def edit_notes(request, note_id):
+    note = get_object_or_404(Note, id=note_id)
+    if request.method == 'POST':
+        form = NoteForm(request.POST,instance=note)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    else:
+        form = NoteForm(instance=note)
+    return render(request, 'notes/edit_note.html', {'form': form, 'note': note})
+
+def delete_note(request, note_id):
+    note = get_object_or_404(request, id=note_id)
+    if request.method == 'POST':
+        note.delete()
+        return(redirect, 'home')
+    return render(request, 'notes/delete_note.html', {'note': note})
