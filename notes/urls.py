5cd72b953be4918c1e86c08af99749a172c05779
iabhinavrajput <abhinavrajput207@gmail.com>
2025-06-25 17:41:40 +0530

    Add URL configuration for notes app with routes for home, add, edit, and delete notes

---
+++ notes/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from .views import views
+
+urlPatterns = [
+    path('', views.home, name='home'),
+    path('add/', views.add_note, name='add-note'),
+    path('edit/<int:note_id>/', views.edit_notes, name='edit-note'),
+    path('delete/<int:note_id>/', views.delete_note, name='delete-note'),
+]
